687456a62d0ed2b657dda0fc755f0a0b57f56edf
xworkzodc <xworkzodc@gmail.com>
2025-03-17 09:41:51 +0530

    Init commit to check git flow

---
+++ boring/src/com/xworkz/boring/Runner.java
@@ -0,0 +1,8 @@
+package com.xworkz.boring;
+//com.xworkz.boring.Runner : FQN : Fully Qualified Name
+public class Runner {
+
+    public static void main(String[] args) {
+        System.out.println("running main Runner");
+    }
+}
